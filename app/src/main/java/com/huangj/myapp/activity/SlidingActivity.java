2c4c30a5a7cce83e64a8e78b1a8f7e1308eb1515
HuangJAndroid <641380764@qq.com>
2017-03-06 14:34:25 +0800

    3.6

--- app/src/main/java/com/huangj/myapp/activity/SlidingActivity.java
+++ app/src/main/java/com/huangj/myapp/activity/SlidingActivity.java
@@ -1,82 +1,52 @@
 package com.huangj.myapp.activity;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import com.huangj.myapp.R;
-import com.huangj.myapp.fragment.MenuLeftFragment;
-import com.nineoldandroids.view.ViewHelper;
+import com.huangj.myapp.view.SlideSideMenuTransitionLayout;
 
-import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
 
 public class SlidingActivity extends AppCompatActivity {
 
-    @ViewInject(R.id.sliding_btn)
-    private ImageView sliding_btn;
-    @ViewInject(R.id.drawerLayout)
-    private DrawerLayout mDrawerLayout;
+    private SlideSideMenuTransitionLayout mSlideSideMenu;
+
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sliding);
         x.view().inject(this);
-        sliding_btn.setOnClickListener(new View.OnClickListener() {
+
+        // Grab the widget
+        mSlideSideMenu = (SlideSideMenuTransitionLayout)findViewById(R.id.slide_side_menu);
+
+        // Setup the toolbar
+        mToolbar = (Toolbar) findViewById(R.id.base_toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+
+        // Wire SideMenu with Toolbar
+        mToolbar.setNavigationIcon(R.drawable.ic_menu);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mSlideSideMenu.toggle();
             }
         });
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.id_left_menu_container);
-        if (fragment == null)
-        {fm.beginTransaction().add(R.id.id_left_menu_container, new MenuLeftFragment()).commit();}
-        mDrawerLayout.setDrawerListener(new DrawerLayout.DrawerListener()
-        {
-            @Override
-            public void onDrawerStateChanged(int newState) {}
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset)
-            {
-                View mContent = mDrawerLayout.getChildAt(0);
-                View mMenu = drawerView;
-                float scale = 1 - slideOffset;
-                float rightScale = 0.8f + scale * 0.2f;
+    }
 
-                if (drawerView.getTag().equals("LEFT"))
-                {
-
-
-                    float leftScale = 1 - 0.3f * scale;
-
-                    ViewHelper.setScaleX(mMenu, leftScale);
-                    ViewHelper.setScaleY(mMenu, leftScale);
-                    ViewHelper.setAlpha(mMenu, 0.6f + 0.4f * (1 - scale));
-                    ViewHelper.setTranslationX(mContent,mMenu.getMeasuredWidth() * (1 - scale));
-                    ViewHelper.setPivotX(mContent, 0);
-                    ViewHelper.setPivotY(mContent,mContent.getMeasuredHeight() / 2);
-                    mContent.invalidate();
-                    ViewHelper.setScaleX(mContent, rightScale);
-                    ViewHelper.setScaleY(mContent, rightScale);
-                }
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView)
-            {
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView)
-            {
-            }
-        });
-
+    @Override
+    public void onBackPressed() {
+        if (mSlideSideMenu != null && mSlideSideMenu.closeSideMenu()) {
+            // Closed the side menu, override the default back pressed behavior
+            return;
+        }
+        super.onBackPressed();
     }
 }
