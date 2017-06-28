ca4c7e509e5c1ea1f51c79ee8ffc6f9093486d82
HuangJAndroid <641380764@qq.com>
2017-06-28 15:52:44 +0800

    6/28

--- app/src/main/java/com/huangj/myapp/activity/InputActivity.java
+++ app/src/main/java/com/huangj/myapp/activity/InputActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -23,6 +22,16 @@ public class InputActivity extends AppCompatActivity {
 
         ((LockableScrollView)findViewById(R.id.scrollview)).setScrollingEnabled(false);
         Log.e("======Input",getIntent().getStringExtra("str"));
+
+        findViewById(R.id.input_textview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("str","inputactivity");
+                setResult(RESULT_CANCELED, InputActivity.this.getIntent().putExtras(bundle));
+                InputActivity.this.finish();
+            }
+        });
     }
 
     @Override
@@ -42,19 +51,5 @@ public class InputActivity extends AppCompatActivity {
             return true;
         }
         return onTouchEvent(ev);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            Bundle bundle = new Bundle();
-            bundle.putString("str","inputactivity");
-            setResult(RESULT_CANCELED, this.getIntent().putExtras(bundle));
-            this.finish();
-            return true;
-        } else {
-            return super.onKeyDown(keyCode, event);
-        }
     }
     }
